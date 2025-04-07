a8852ddf172c3045a55d96dad5b0d5bb578f745a
Ginkgo <leisun1101@gmail.com>
2025-04-07 17:02:28 +0200

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,18 +1,21 @@
 'use strict'
 
-import { app, protocol, BrowserWindow, globalShortcut, ipcMain } from 'electron'
+import { app, protocol, BrowserWindow, globalShortcut, ipcMain, nativeTheme } from 'electron'
 import fs from 'fs'
 import path from 'path'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
+const theme = process.env.DARK_THEME ? 'dark' : 'light'
+
+nativeTheme.themeSource = theme
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-const SAVE_FILE_PATH = path.join(app.getPath('home'), '.memo.md')
+const SAVE_FILE_PATH = process.env.MEMOFILEPATH ? process.env.MEMOFILEPATH : path.join(app.getPath('home'), '.memo.md')
 
 async function createWindow() {
   // Create the browser window.
@@ -21,7 +24,9 @@ async function createWindow() {
     height: 560,
     minWidth: 560,
     minHeight: 560,
-    title: 'Memo Simple Client',
+    title: process.env.MEMOTITLE ? process.env.MEMOTITLE : 'Memo',
+    darkTheme: process.env.THEME === 'dark',
+    backgroundColor:  process.env.THEME === 'dark' ? '#25292e' : '#FFFFFF',
     webPreferences: {
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
@@ -43,11 +48,13 @@ async function createWindow() {
     console.warn('failed to read the .memo.md file:', err)
   }
 
+
   win.webContents.on('did-finish-load', () => {
     win.webContents.send('load-content', initialContent)
+    win.webContents.send('theme', process.env.THEME);
   })
 
-  globalShortcut.register('CommandOrControl+S', () => {
+  globalShortcut.register('CommandOrControl+Enter', () => {
     win.webContents.send('save-content')
   })
 
@@ -62,7 +69,7 @@ async function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    // if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
